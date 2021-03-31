a117b686a7f1673236025fe6538d7de4a1e07f08
slwu89 <10673535+slwu89@users.noreply.github.com>
2021-03-31 11:23:18 -0700

    simple shiny app

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,11 +1,51 @@
-server <- function(input, output, session) {
-  output$summary <- shiny::renderPrint({
-    dataset <- get(input$dataset, "package:datasets")
-    summary(dataset)
-  })
+# server <- function(input, output, session) {
+#   
+#   # Create a reactive expression
+#   dataset <- shiny::reactive({
+#     get(input$dataset, "package:datasets")
+#   })
+# 
+#   output$summary <- shiny::renderPrint({
+#     summary(dataset())
+#   })
+# 
+#   output$table <- shiny::renderTable({
+#     dataset()
+#   })
+# }
+
+
+  freqpoly <- function(x1, x2, binwidth = 0.1, xlim = c(-3, 3)) {
+    df <- data.frame(
+      x = c(x1, x2),
+      g = c(rep("x1", length(x1)), rep("x2", length(x2)))
+    )
+    
+    ggplot(df, aes(x, colour = g)) +
+      geom_freqpoly(binwidth = binwidth, size = 1) +
+      coord_cartesian(xlim = xlim)
+  }
   
-  output$table <- shiny::renderTable({
-    dataset <- get(input$dataset, "package:datasets")
-    dataset
-  })
-}
+  t_test <- function(x1, x2) {
+    test <- t.test(x1, x2)
+    
+    # use sprintf() to format t.test() results compactly
+    sprintf(
+      "p value: %0.3f\n[%0.2f, %0.2f]",
+      test$p.value, test$conf.int[1], test$conf.int[2]
+    )
+  }
+  
+  server <- function(input, output, session) {
+    x1 <- reactive({
+      input$simulate
+      rpois(input$n, input$lambda1)
+    })
+    x2 <- reactive({
+      input$simulate
+      rpois(input$n, input$lambda2)
+    })
+    output$hist <- renderPlot({
+      freqpoly(x1(), x2(), binwidth = 1, xlim = c(0, 40))
+    }, res = 96)
+  }
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,5 +1,20 @@
 ui <- shiny::fluidPage(
-  shiny::selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-  shiny::verbatimTextOutput("summary"),
-  shiny::tableOutput("table")
+  shiny::fluidRow(
+    shiny::column(3, 
+           shiny::numericInput("lambda1", label = "lambda1", value = 3),
+           shiny::numericInput("lambda2", label = "lambda2", value = 5),
+           shiny::numericInput("n", label = "n", value = 1e4, min = 0),
+           shiny::actionButton("simulate", "Simulate!")
+    ),
+    shiny::column(9, plotOutput("hist"))
+  )
 )
+
+# ui <- shiny::fluidPage(
+#   # shiny::fileInput("upload", NULL) # this is how we'll input files
+#   shiny::fluidRow(
+#     shiny::selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+#     shiny::verbatimTextOutput("summary"),
+#     shiny::tableOutput("table")
+#   )
+# )
