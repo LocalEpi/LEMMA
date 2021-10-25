263e9a611ddf2a0f0e36a9356ec248aec520fc39
joshuaschwab <jschwab77@gmail.com>
2021-10-25 10:16:43 -0700

    increase date range for short term plots

--- R/CreateOutputs.R
+++ R/CreateOutputs.R
@@ -18,13 +18,13 @@ GetExcelOutputData <- function(projection, fit, inputs) {
   options("openxlsx.numFmt" = "0.00")
   display.date.range <- seq(inputs$model.inputs$start.display.date, inputs$model.inputs$end.date, by = "day")
   output.list <- list(projection = projection[date %in% display.date.range])
-  
+
   pars <- c("r0", "duration_latent", "duration_rec_mild", "duration_pre_hosp", "duration_hosp_mod",
             "duration_hosp_icu", "duration_mort_nonhosp",  "frac_hosp", "frac_icu", "frac_mort", "frac_mort_nonhosp" , "frac_tested",
             "ini_exposed")
   output.list$posteriorParams <- data.table(param = pars, posterior = unlist(fit$par[pars]))
   output.list$posteriorInterventions <- data.table(beta_multiplier = as.vector(fit$par$beta_multiplier), t_inter = as.character(fit$par$t_inter + inputs$internal.args$simulation.start.date), len_inter = as.vector(fit$par$len_inter))
-  
+
   output.list$all.inputs = inputs$all.inputs.str
   output.list$all.inputs.asrun <- inputs$all.inputs.str.asrun
   return(output.list)
@@ -69,7 +69,7 @@ GetProjectionPlot <- function(short.term, projection, data.type, inputs) {
   dt.plot <- merge(obs.data, projection.dt, all = T, by = "date")
 
   if (short.term) {
-    max.date <- obs.data[!is.na(conf), max(date)] + 3
+    max.date <- obs.data[!is.na(conf), max(date)] + 20
     title1 <- paste("Short Term", GetTitleLabel(data.type), "Projection")
     plot.observed.data <- inputs$internal.args$plot.observed.data.short.term
   } else {
@@ -162,7 +162,7 @@ GetPdfOutputPlots <- function(fit, projection, inputs) {
     long.term[[i]] <- GetProjectionPlot(short.term = F, projection = projection, data.type = i, inputs = inputs)
     if (!is.null(long.term[[i]])) print(long.term[[i]])
   }
-  
+
   rt.plot <- GetRtPlot(projection, inputs)
   return(list(short.term = short.term, long.term = long.term, rt = rt.plot))
 }
@@ -170,12 +170,12 @@ GetPdfOutputPlots <- function(fit, projection, inputs) {
 GetPdfOutput <- function(fit, projection, inputs) {
   devlist <- grDevices::dev.list()
   sapply(devlist[names(devlist) == "pdf"], grDevices::dev.off) #shuts down any old pdf (if there was a crash part way)
-  
+
   filestr.out <- paste0(inputs$internal.args$output.filestr, ".pdf")
   grDevices::pdf(file = filestr.out, width = 9.350, height = 7.225)
-  
+
   plots <- GetPdfOutputPlots(fit,projection,inputs)
-  
+
   grDevices::dev.off()
   cat("\nPDF output: ", filestr.out, "\n")
   return(plots)
@@ -184,10 +184,10 @@ GetPdfOutput <- function(fit, projection, inputs) {
 # GetPdfOutput <- function(fit, projection, inputs) {
 #   devlist <- grDevices::dev.list()
 #   sapply(devlist[names(devlist) == "pdf"], grDevices::dev.off) #shuts down any old pdf (if there was a crash part way)
-#   
+#
 #   filestr.out <- paste0(inputs$internal.args$output.filestr, ".pdf")
 #   grDevices::pdf(file = filestr.out, width = 9.350, height = 7.225)
-#   
+#
 #   short.term <- long.term <- list()
 #   for (i in DataTypes()) {
 #     if (i == "seroprev" & inputs$internal.args$hide.nonpublic.data) {
@@ -199,7 +199,7 @@ GetPdfOutput <- function(fit, projection, inputs) {
 #     long.term[[i]] <- GetProjectionPlot(short.term = F, projection = projection, data.type = i, inputs = inputs)
 #     if (!is.null(long.term[[i]])) print(long.term[[i]])
 #   }
-#   
+#
 #   rt.plot <- GetRtPlot(projection, inputs)
 #   grDevices::dev.off()
 #   cat("\nPDF output: ", filestr.out, "\n")
